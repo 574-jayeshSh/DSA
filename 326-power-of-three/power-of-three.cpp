0c7631060cf4c8bef27c84697fcba02cfbc34adc
Jayesh Sharma <574jayesh.sharma@gmail.com>
2025-08-13 22:56:23 +0530

    Time: 3 ms (60.14%) | Memory: 8.8 MB (76.82%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
   bool isPowerOfThree(int n) {
     if (n <= 0) return false;
-    for (long long i = 1; i <= n; i *= 3) {
-        if (i == n) return true;
+    while(n%3 == 0){
+        n /= 3;
     }
-    return false;
-}
+
+    return n ==1;}
 };
