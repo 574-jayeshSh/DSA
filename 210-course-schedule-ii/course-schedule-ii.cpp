263d50ed8b65afd452b766c5211325f9d15c97fc
Jayesh Sharma <574jayesh.sharma@gmail.com>
2025-09-04 23:27:44 +0530

    Time: 7 ms (32.49%) | Memory: 18 MB (89.91%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -3,54 +3,34 @@ using namespace std;
 
 class Solution {
 public:
-    bool check(int node,
-               unordered_map<int, bool>& visited,
-               unordered_map<int, bool>& dfsvisited,
-               unordered_map<int, list<int>>& adj,
-               vector<int>& ans) {
-        
-        visited[node] = true;
-        dfsvisited[node] = true;
-
-        for (auto neighbor : adj[node]) {
-            if (!visited[neighbor]) {
-                if (check(neighbor, visited, dfsvisited, adj, ans)) {
-                    return true; // cycle found
-                }
-            } 
-            else if (dfsvisited[neighbor]) {
-                return true; // cycle detected
-            }
-        }
-
-        dfsvisited[node] = false; // backtrack
-        ans.push_back(node);      // add to topo order
-        return false;
-    }
 
     vector<int> findOrder(int numCourses, vector<vector<int>>& edges) {
-        unordered_map<int, list<int>> adj;
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
 
-        // build adjacency list
-        for (int i = 0; i < edges.size(); i++) {
-            int a = edges[i][0];
-            int b = edges[i][1];
-            adj[b].push_back(a);
+        for (auto &p : edges) {
+            adj[p[1]].push_back(p[0]);
+            indegree[p[0]]++;
         }
 
-        vector<int> ans;
-        unordered_map<int, bool> visited;
-        unordered_map<int, bool> dfsvisited;
-
+        queue<int> q;
         for (int i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                if (check(i, visited, dfsvisited, adj, ans)) {
-                    return {}; // cycle → no valid order
-                }
+            if (indegree[i] == 0) q.push(i);
+        }
+
+        vector<int> ans; // number of courses we can complete
+        while (!q.empty()) {
+            int node = q.front(); q.pop();
+            ans.push_back(node);
+
+            for (int neighbor : adj[node]) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0)
+                    q.push(neighbor);
             }
         }
-
-        reverse(ans.begin(), ans.end()); // reverse for correct topo order
-        return ans;
+        
+        if(ans.size() == numCourses) return ans;
+        else return {};
     }
 };
