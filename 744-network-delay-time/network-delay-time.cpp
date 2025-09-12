c5e7ff51458c53a7d3d363e2234e7f172667d41a
Jayesh Sharma <574jayesh.sharma@gmail.com>
2025-09-12 20:55:14 +0530

    Time: 80 ms (74.18%) | Memory: 46.2 MB (46.59%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    struct cell{
+        int time , src;
+        bool operator<(const cell& other) const {
+            return time > other.time;
+        }
+    };
+
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        unordered_map<int , list<pair<int,int>> >adj;
+        for(int i = 0; i<times.size(); i++){
+            int src = times[i][0];
+            int tar = times[i][1];
+            int time = times[i][2];
+
+            adj[src].push_back({tar,time});
+        }
+
+        priority_queue<cell> q;
+        q.push({0,k});
+        vector<bool> visited(n);
+        vector<int> timeArr(n,INT_MAX);
+        timeArr[k-1] = 0;
+
+        while(!q.empty()){
+            auto [time,node] = q.top();
+            q.pop();
+
+            if (visited[node-1]) continue;
+            visited[node-1] = true;
+            
+            for(auto it : adj[node]){
+                int next = it.first;
+                int timeToReach = it.second;
+                if(timeToReach + time < timeArr[next-1]){
+                    timeArr[next-1] = timeToReach + time;
+                    q.push({timeArr[next - 1] , next});
+                }
+            }
+        }
+
+        int tym = INT_MIN;
+        for(int i = 0; i<n; i++){
+            if(visited[i]){
+                tym = max(tym,timeArr[i]);
+            }
+        }
+
+        
+        for(int i = 0; i<n; i++){
+            if(!visited[i]){
+                return -1;
+            }
+        }
+        return tym;
+    }
+};
