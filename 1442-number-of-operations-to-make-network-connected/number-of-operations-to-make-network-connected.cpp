96d81af1522175790970bad58613b53138520fe7
Jayesh Sharma <574jayesh.sharma@gmail.com>
2025-09-15 23:10:05 +0530

    Time: 22 ms (45.50%) | Memory: 42.1 MB (92.52%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+    int find(int i, vector<int> &parent){
+        return (parent[i] == i) ? i : (parent[i] = find(parent[i],parent));
+    }
+    
+    void unite(int x , int y , vector<int> &parent, vector<int> &rank){
+        int s1 = find(x,parent) , s2 = find(y,parent);
+
+        if(s1 != s2){
+            if(rank[s1] < rank[s2]) parent[s1] = s2;
+            else if(rank[s2] < rank[s1]) parent[s2] = s1;
+            else parent[s2] = s1, rank[s1]++;
+        }
+    }
+
+    
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size() < n-1) return -1;
+
+        sort(connections.begin(), connections.end(),
+            [](vector<int>& a, vector<int>& b) {
+                return a[0] < b[0];
+            });
+        vector<int> parent(n), rank(n);
+        for(int i = 0; i<n; i++){
+            parent[i] = i;
+            rank[i] = 1;
+        }
+
+        int count = 0;
+        int skip = 0;
+        for(auto &e : connections){
+            int x = e[0] , y = e[1];
+            if(find(x,parent) != find(y,parent)){
+                unite(x,y,parent,rank);
+                if(count++ == n-1) break;
+            }
+            else{
+                skip++;
+                continue;
+            }
+        }
+        
+        int components = 0;
+        for(int i = 0; i<n; i++){
+            if(find(i,parent) == i) components++;
+        }
+        return components - 1 ;
+    }
+};
