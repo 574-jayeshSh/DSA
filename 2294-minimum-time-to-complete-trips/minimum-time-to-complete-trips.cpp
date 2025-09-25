23f5c523bb30e140587f1661a3c86b711d77f6ac
Jayesh Sharma <574jayesh.sharma@gmail.com>
2025-09-25 16:12:18 +0530

    Time: 35 ms (94.41%) | Memory: 98.5 MB (50.80%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
@@ -3,15 +3,16 @@ public:
     long long trip(long long mid , vector<int>& time){
         long long total = 0;
         for(auto i : time){
-            total += ( mid / i);
+            total += ( mid /i);
         }
         return total;
     }
     long long minimumTime(vector<int>& time, int totalTrips) {
         
-        sort(time.begin(), time.end());
+        
         int n = time.size();
-        long long low  = 1, high = (long long)  time[n-1] * totalTrips;
+        long long low  = 1, high = (long long)*min_element(time.begin(), time.end()) * totalTrips;
+
 
         while(low < high){
             long long mid = (low + high)/2;
