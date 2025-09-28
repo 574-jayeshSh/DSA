06aed9a22750a537ffa12221f3cb3059e2c6e8ba
Jayesh Sharma <574jayesh.sharma@gmail.com>
2025-09-29 01:23:15 +0530

    Time: 15 ms (92.80%) | Memory: 59.7 MB (88.34%) - LeetSync

--- 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    bool check(long long time ,vector<int>& batteries , int n){
+    long long maxRunTime(int n, vector<int>& batteries) {
+        long long left = *min_element(batteries.begin(), batteries.end());
         long long sum = 0;
         for(int i = 0; i<batteries.size(); i++){
-            sum += min((long long)batteries[i], time);
-            if(sum >= n*time) return true;
+            sum += batteries[i];
         }
-        return false;
-    }
-    long long maxRunTime(int n, vector<int>& batteries) {
-         long long left = 0;
-        long long sum = 0;
-        for(int b : batteries) sum += b;
-        long long right = sum / n;
+        long long right = sum/n;
         long long answer = 0;
+        while(left <= right){
+            long long mid = (left+right) / 2;
+            long long total = 0;
+            for(int b : batteries) total += min((long long)b, mid);
 
-        while(left <= right) {
-            long long mid = left + (right - left) / 2;
-            if(check(mid, batteries, n)) {
+            if(n*mid <= total){
                 answer = mid;
                 left = mid + 1;
-            } else {
+            }
+            else{
                 right = mid - 1;
             }
         }
-
         return answer;
 
     }
