b3d9abed4258f4173981c3924d5fa972d0fb4d9b
Jayesh Sharma <574jayesh.sharma@gmail.com>
2025-09-29 00:05:11 +0530

    Time: 63 ms (83.67%) | Memory: 105.4 MB (83.27%) - LeetSync

--- 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.cpp
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    double time(int speed ,vector<int>& dist){
-        double t = 0;
-        for(int i = 0; i<dist.size(); i++){
-            if(i != dist.size()-1){
-                t += ceil((double)dist[i]/speed);
-            }
-            else t += (double)dist[i]/speed;
-        }
-        return t;
-    }
     int minSpeedOnTime(vector<int>& dist, double hour) {
-        int right = 1e7;
-        int left = 1;
-        int answer = 0;
-        while(left <= right){
+        int n = dist.size();
+        if(hour <= n - 1) return -1;
+
+        int left = 1, right = 1e7, answer = -1;
+
+        while(left <= right) {
             int mid = left + (right - left) / 2;
-            
-            if(hour >= time(mid , dist)){
+            double t = 0;
+
+            for(int i = 0; i < n; i++) {
+                if(i != n - 1)
+                    t += (dist[i] + mid - 1) / mid; // integer ceil trick
+                else
+                    t += (double)dist[i] / mid;
+            }
+
+            if(t <= hour) {
                 answer = mid;
                 right = mid - 1;
-            }
-            else{
+            } else {
                 left = mid + 1;
             }
         }
-        return answer == 0 ? -1 : answer;
 
+        return answer;
     }
 };
