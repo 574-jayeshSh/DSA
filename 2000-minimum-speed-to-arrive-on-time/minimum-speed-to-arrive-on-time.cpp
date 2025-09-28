6abe58885e68d99cd63dcf351cfc637d172c0958
Jayesh Sharma <574jayesh.sharma@gmail.com>
2025-09-29 00:01:11 +0530

    Time: 79 ms (47.25%) | Memory: 105.4 MB (54.90%) - LeetSync

---
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    double time(int speed ,vector<int>& dist){
+        double t = 0;
+        for(int i = 0; i<dist.size(); i++){
+            if(i != dist.size()-1){
+                t += ceil((double)dist[i]/speed);
+            }
+            else t += (double)dist[i]/speed;
+        }
+        return t;
+    }
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        int right = 1e7;
+        int left = 1;
+        int answer = 0;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            
+            if(hour >= time(mid , dist)){
+                answer = mid;
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return answer == 0 ? -1 : answer;
+
+    }
+};
