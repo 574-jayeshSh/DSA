c640f9907eaea12551da7cfd6a986e46225afd39
Jayesh Sharma <574jayesh.sharma@gmail.com>
2025-09-29 22:17:36 +0530

    Time: 141 ms (85.59%) | Memory: 169.2 MB (85.35%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
@@ -1,54 +1,34 @@
 class Solution {
 public:
     void dfs(int node, int parent, int &timer, vector<int> &disc, vector<int> &low,
-        unordered_map<int , list<int> >&adj, unordered_map<int , bool> &vis,vector<vector<int>> &result){
-
-            vis[node] = 1;
-            disc[node] = low[node] = timer++;
-
-            for(auto nbr : adj[node]){
-                if(nbr == parent) continue;
-
-                if(!vis[nbr]){
-                    dfs(nbr,node,timer,disc,low,adj,vis,result);
-                    low[node] = min(low[node], low[nbr]);
-                    //check bridge
-                    if(low[nbr] > disc[node]){
-                        vector<int> ans;
-                        ans.push_back(node);
-                        ans.push_back(nbr);
-                        result.push_back(ans);
-                    }
-                }
-                else{
-                    // backedge
-                    low[node] = min(low[node],disc[nbr]);
-                }
+             vector<vector<int>> &adj, vector<bool> &vis, vector<vector<int>> &result) {
+        vis[node] = true;
+        disc[node] = low[node] = timer++;
+        for (auto nbr : adj[node]) {
+            if (nbr == parent) continue;
+            if (!vis[nbr]) {
+                dfs(nbr, node, timer, disc, low, adj, vis, result);
+                low[node] = min(low[node], low[nbr]);
+                if (low[nbr] > disc[node]) result.push_back({node, nbr});
+            } else {
+                low[node] = min(low[node], disc[nbr]);
             }
+        }
     }
+
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        unordered_map<int , list<int> >adj;
-        for(int i = 0; i<connections.size(); i++){
-            int u = connections[i][0];
-            int v = connections[i][1];
-
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        vector<vector<int>> adj(n);
+        for (auto &c : connections) {
+            adj[c[0]].push_back(c[1]);
+            adj[c[1]].push_back(c[0]);
         }
-
         int timer = 0;
-        vector<int> disc(n,-1);
-        vector<int> low(n,-1);
-        int parent = -1;
-        unordered_map<int , bool> vis;
+        vector<int> disc(n, -1), low(n, -1);
+        vector<bool> vis(n, false);
         vector<vector<int>> result;
-
-        for(int i = 0; i<n; i++){
-            if(!vis[i]){
-                dfs(i,parent, timer, disc, low, adj, vis, result);
-            }
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) dfs(i, -1, timer, disc, low, adj, vis, result);
         }
-
         return result;
     }
 };
